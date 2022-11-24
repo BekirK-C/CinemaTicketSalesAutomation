1e45e2e72d8485477c33c09ede2ce9c66f86ab88
BekirK-C <b.b.kamac@gmail.com>
2022-11-25 00:14:25 +0300

    Creating models and contructors

---
+++ CinemaTicketSalesAutomation/Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using CinemaTicketSalesAutomation.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSalesAutomation.Models
+{
+    public class Movie : BaseModel
+    {
+        public Movie()
+        {
+            SetDefaultSessions();
+        }
+
+        public string picturePath { get; set; }
+        public string minute { get; set; }
+        public decimal price { get; set; }
+        public Category category { get; set; }
+        public List<Session> sessions { get; set; }
+
+        private void SetDefaultSessions()
+        {
+            sessions = new List<Session>();
+            DateTime currentDate = DateTime.Now;
+            TimeSpan timeSpan = new TimeSpan(10, 30, 0);
+
+            for (int i = 0; i < 3; i++)
+            {
+                currentDate = currentDate.Date + timeSpan;
+                for (int j = 0; j < 3; j++)
+                {
+                    Session session = new Session();
+                    session.date = currentDate.ToShortDateString();
+                    session.time = currentDate.ToShortTimeString();
+                    sessions.Add(session);
+                    currentDate.AddHours(3);
+                }
+                currentDate.AddDays(1);
+            }
+        }
+    }
+}
