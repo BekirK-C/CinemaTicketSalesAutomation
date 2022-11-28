18f4360573815bf80517a06f0199b574c60465ef
BekirK-C <b.b.kamac@gmail.com>
2022-11-28 19:45:35 +0300

    Film Detaylarının Oluşturulması

--- CinemaTicketSalesAutomation/Form1.Designer.cs
+++ CinemaTicketSalesAutomation/Form1.Designer.cs
@@ -28,23 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblDate = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDate.Location = new System.Drawing.Point(514, 23);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 36);
+            this.lblDate.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1182, 673);
+            this.Controls.Add(this.lblDate);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDate;
     }
 }
 
---
+++ CinemaTicketSalesAutomation/Form2.cs
@@ -0,0 +1,37 @@
+﻿using CinemaTicketSalesAutomation.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaTicketSalesAutomation
+{
+    public partial class Form2 : Form
+    {
+        List<Movie> movies;
+        Form form1;
+        Movie selectedMovie;
+        Session selectedSession;
+        public Form2(List<Movie> _movies, Form  _form1)
+        {
+            InitializeComponent();
+            movies= _movies;
+            form1= _form1;
+        }
+        public void ListDetail(int movieIndex, string _time, string _date)
+        {
+            selectedMovie = movies[movieIndex];
+            selectedSession = selectedMovie.sessions.Find(s => s.date == _date && s.time == _time);
+            lblTime.Text = $"{_date} - {_time}";
+            lblMinute.Text = selectedMovie.minute;
+            lblPrice.Text = selectedMovie.price.ToString() + "TL";
+            pbSelectedPicture.Image = Image.FromFile(selectedMovie.picturePath);
+            lblCategory.Text = selectedMovie.category.ToString();
+        }
+    }
+}
